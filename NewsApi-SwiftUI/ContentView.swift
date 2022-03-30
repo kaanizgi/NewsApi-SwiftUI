b68d41277c18960990dd04451b22e4dc5a975afd
Kaan İzgi <kaanizgi1531@gmail.com>
2022-03-30 23:09:33 +0300

    Initial Commit

---
+++ NewsApi-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsApi-SwiftUI
+//
+//  Created by Kaan İzgi on 30.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsApi-SwiftUI/NewsApi_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApi_SwiftUIApp.swift
+//  NewsApi-SwiftUI
+//
+//  Created by Kaan İzgi on 30.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsApi_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
